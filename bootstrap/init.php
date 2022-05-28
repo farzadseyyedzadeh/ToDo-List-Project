eb9f63fe4374451bdcbd8c10ad88267a82b356a4
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-05-29 00:04:11 +0430

    trim the path

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 include "constants.php";
-include BASE_PATH . "bootstrap/config.php";
-include BASE_PATH . "vendor/autoload.php";
-include BASE_PATH . "libs/helpers.php";
+include BASE_PATH . "/bootstrap/config.php";
+include BASE_PATH . "/vendor/autoload.php";
+include BASE_PATH . "/libs/helpers.php";
 
 try {
     $pdo = new PDO("mysql:dbname=$database_config->db;host={$database_config->host}", $database_config->user, $database_config->pass);
@@ -11,8 +11,8 @@ try {
     diePage('Connection failed: ' . $e->getMessage());
 }
 
-include BASE_PATH . "libs/lib-auth.php";
-include BASE_PATH . "libs/lib-tasks.php";
+include BASE_PATH . "/libs/lib-auth.php";
+include BASE_PATH . "/libs/lib-tasks.php";
 
 // echo "Connection to Database is OK!";
 
