3c0e8046d534d59aae565d735333750914766d52
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-06-01 23:22:28 +0430

    final versin

--- bootstrap/constants.php
+++ bootstrap/constants.php
@@ -1,5 +1,5 @@
 <?php
-define('SITE_TITLE','ToDo-List-Project');
+define('SITE_TITLE','7Task Project');
 define('BASE_URL','http://localhost/ToDo-list-Project/');
 define('BASE_PATH','C:\xampp\htdocs\ToDo-List-Project');
 
