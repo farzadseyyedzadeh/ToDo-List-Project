9a590f90dc16e8ff3dbd40c466d44e7e155b8ce0
farzad seyyedzadeh <iamseyfarsey@gmail.com>
2022-06-07 23:05:46 +0430

    7.6.2022

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -5,11 +5,13 @@ include BASE_PATH . "/bootstrap/config.php";
 include BASE_PATH . "/vendor/autoload.php";
 include BASE_PATH . "/libs/helpers.php";
 
+
 try {
     $pdo = new PDO("mysql:dbname=$database_config->db;host={$database_config->host}", $database_config->user, $database_config->pass);
 } catch (PDOException $e) {
     diePage('Connection failed: ' . $e->getMessage());
 }
+
 
 include BASE_PATH . "/libs/lib-auth.php";
 include BASE_PATH . "/libs/lib-tasks.php";
